092cb8c726248f4c597e1aa1ccbae9f7e61a5b6d
Axel Svensson <mail@axelsvensson.com>
2021-12-10 13:04:22 +0100

    svenssonaxel: Lower pinky columns

--- keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
+++ keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
@@ -50,21 +50,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    SE_SECT,   KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_F1,                               KC_F2,     KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      SE_PLUS,   //
-    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_F3,                               KC_F4,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      SE_AA,     //
-    KC_F5,     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,                                                      KC_H,      KC_J,      KC_K,      KC_L,      SE_OE,     SE_AE,     //
-    SE_LESS,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_F6,                               KC_F7,     KC_N,      KC_M,      KC_COMMA,  KC_DOT,    SE_MINS,   KC_RSHIFT, //
-    KC_F9,     KC_F10,    KC_F11,    KC_F12,    MO(1),                                                                           KC_F13,    KC_F14,    KC_F15,    KC_F16,    LOCKDESK,  //
+    LOCKDESK,  XXXXXXX,   KC_2,      KC_3,      KC_4,      KC_5,      KC_F1,                               KC_F2,     KC_6,      KC_7,      KC_8,      KC_9,      XXXXXXX,   LOCKDESK,  //
+    SE_SECT,   KC_1,      KC_W,      KC_E,      KC_R,      KC_T,      KC_F3,                               KC_F4,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_0,      SE_PLUS,   //
+    KC_TAB,    KC_Q,      KC_S,      KC_D,      KC_F,      KC_G,                                                      KC_H,      KC_J,      KC_K,      KC_L,      KC_P,      SE_AA,     //
+    KC_F5,     KC_A,      KC_X,      KC_C,      KC_V,      KC_B,      KC_F6,                               KC_F7,     KC_N,      KC_M,      KC_COMMA,  KC_DOT,    SE_OE,     SE_AE,     //
+    SE_LESS,   KC_Z,      KC_F11,    KC_F12,    MO(1),                                                                           KC_F13,    KC_F14,    KC_F15,    SE_MINS,   KC_RSHIFT, //
                                                                       KC_F17,    KC_F18,        KC_F19,    KC_F20,                                                                      //
                                                                                  KC_F21,        KC_F22,                                                                                 //
                                                            KC_BSPACE, KC_DELETE, KC_F23,        KC_F24,    KC_ENTER,  KC_SPACE                                                          //
   ),
   [1] = LAYOUT_ergodox_pretty(
-    KC_ESC,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                             XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   LEDBRIGHT, XXXXXXX,   RESET,     //
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                             XXXXXXX,   XXXXXXX,   XXXXXXX,   MS_UP,     XXXXXXX,   MS_BTN2,   XXXXXXX,   //
-    MS_ACCEL2, MS_ACCEL1, MS_ACCEL0, MS_SLOW,   MS_SLOWER, XXXXXXX,                                                   MSW_UP,    MS_LEFT,   MS_DOWN,   MS_RIGHT,  MS_BTN1,   XXXXXXX,   //
-    XXXXXXX,   CONNLC,    CONNLCBC,  CONND,     CONNDBC,   CYGWIN,    XXXXXXX,                             XXXXXXX,   MSW_DOWN,  MSW_LEFT,  XXXXXXX,   MSW_RIGHT, MS_BTN3,   XXXXXXX,   //
-    KC_LSUPER, XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                         LOCKKBD,   UNLOCKKBD, XXXXXXX,   XXXXXXX,   LOCKDESK,  //
+    LOCKDESK,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                             XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   LEDBRIGHT, RESET,     LOCKDESK,  //
+    KC_ESC,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                             XXXXXXX,   XXXXXXX,   XXXXXXX,   MS_UP,     XXXXXXX,   XXXXXXX,   XXXXXXX,   //
+    XXXXXXX,   XXXXXXX,   MS_ACCEL0, MS_SLOW,   MS_SLOWER, XXXXXXX,                                                   MSW_UP,    MS_LEFT,   MS_DOWN,   MS_RIGHT,  MS_BTN2,   XXXXXXX,   //
+    MS_ACCEL2, MS_ACCEL1, CONNLCBC,  CONND,     CONNDBC,   CYGWIN,    XXXXXXX,                             XXXXXXX,   MSW_DOWN,  MSW_LEFT,  XXXXXXX,   MSW_RIGHT, MS_BTN1,   XXXXXXX,   //
+    KC_LSUPER, CONNLC,    XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                         LOCKKBD,   UNLOCKKBD, XXXXXXX,   MS_BTN3,   XXXXXXX,   //
                                                                       XXXXXXX,   XXXXXXX,       XXXXXXX,   XXXXXXX,                                                                     //
                                                                                  XXXXXXX,       OSM_LALT,                                                                               //
                                                            XXXXXXX,   XXXXXXX,   XXXXXXX,       OSM_LCTL,  EXITVNCV,  ALTTAB                                                            //
