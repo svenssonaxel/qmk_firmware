83391a79620a497345216d84c51f5370616fc85c
Axel Svensson <mail@axelsvensson.com>
2022-03-20 20:46:32 +0100

    svenssonaxel: Add two buttons for LOCKKBD

--- keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
+++ keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
@@ -50,7 +50,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    LOCKDESK,  XXXXXXX,   KC_2,      KC_3,      KC_4,      KC_5,      KC_F1,                               KC_F2,     KC_6,      KC_7,      KC_8,      KC_9,      XXXXXXX,   LOCKDESK,  //
+    LOCKDESK,  LOCKKBD,   KC_2,      KC_3,      KC_4,      KC_5,      KC_F1,                               KC_F2,     KC_6,      KC_7,      KC_8,      KC_9,      LOCKKBD,   LOCKDESK,  //
     SE_SECT,   KC_1,      KC_W,      KC_E,      KC_R,      KC_T,      KC_F3,                               KC_F4,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_0,      SE_PLUS,   //
     KC_TAB,    KC_Q,      KC_S,      KC_D,      KC_F,      KC_G,                                                      KC_H,      KC_J,      KC_K,      KC_L,      KC_P,      SE_AA,     //
     KC_F5,     KC_A,      KC_X,      KC_C,      KC_V,      KC_B,      KC_F6,                               KC_F7,     KC_N,      KC_M,      KC_COMMA,  KC_DOT,    SE_OE,     SE_AE,     //
