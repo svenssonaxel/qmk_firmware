68ebb793efb275707a4ee5067807ca39fcf11c11
Axel Svensson <mail@axelsvensson.com>
2024-10-11 23:56:36 +0200

    svenssonaxel: C+A+A and Down (for keepass)

--- keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
+++ keyboards/ergodox_ez/keymaps/svenssonaxel/keymap.c
@@ -66,10 +66,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty( // Remote mode: Base
+#define C_A_A LCTL(LALT(KC_A))
     LOCKDESK,  LOCKKBD,   KC_2,      KC_3,      KC_4,      KC_5,      KC_F1,                               KC_F2,     KC_6,      KC_7,      KC_8,      KC_9,      LOCKKBD,   LOCKDESK,  //
-    SE_SECT,   KC_1,      KC_W,      KC_E,      KC_R,      KC_T,      KC_F3,                               KC_F4,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_0,      SE_PLUS,   //
+    SE_SECT,   KC_1,      KC_W,      KC_E,      KC_R,      KC_T,      C_A_A,                               KC_F4,     KC_Y,      KC_U,      KC_I,      KC_O,      KC_0,      SE_PLUS,   //
     KC_TAB,    KC_Q,      KC_S,      KC_D,      KC_F,      KC_G,                                                      KC_H,      KC_J,      KC_K,      KC_L,      KC_P,      SE_AA,     //
-    KC_F5,     KC_A,      KC_X,      KC_C,      KC_V,      KC_B,      KC_F6,                               TO(1),     KC_N,      KC_M,      KC_COMMA,  KC_DOT,    SE_OE,     SE_AE,     //
+    KC_F5,     KC_A,      KC_X,      KC_C,      KC_V,      KC_B,      KC_DOWN,                             TO(1),     KC_N,      KC_M,      KC_COMMA,  KC_DOT,    SE_OE,     SE_AE,     //
     SE_LESS,   KC_Z,      KC_F11,    KC_F12,    MO_FN,                                                                           KC_F13,    KC_F14,    KC_F15,    SE_MINS,   KC_RSFT,   //
                                                                       KC_F17,    KC_F18,        KC_F19,    KC_F20,                                                                      //
                                                                                  KC_F21,        KC_F22,                                                                                 //
